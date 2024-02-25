d6d0743bd86111d0ee51f94a4e5b6468115ccbe0
Luan Verdelho <luanverdelho642@gmail.com>
2024-02-25 20:24:51 -0300

    feat: create model for the database

---
+++ src/main/java/com/banksimplifiedbackend/BankSimplifiedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.banksimplifiedbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSimplifiedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankSimplifiedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banksimplifiedbackend/BankSimplifiedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banksimplifiedbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankSimplifiedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
