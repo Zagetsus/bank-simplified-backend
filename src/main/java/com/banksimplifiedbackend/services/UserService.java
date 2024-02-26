0b791a9dc3132af3896d2ca2d2117b73e5cceaf3
Luan Verdelho <luanverdelho642@gmail.com>
2024-02-26 11:51:49 -0300

    feat: creation of user and transaction services

--- src/main/java/com/banksimplifiedbackend/services/UserService.java
+++ src/main/java/com/banksimplifiedbackend/services/UserService.java
@@ -1,2 +1,33 @@
-package com.banksimplifiedbackend.services;public class UserService {
+package com.banksimplifiedbackend.services;
+
+import com.banksimplifiedbackend.domain.user.User;
+import com.banksimplifiedbackend.domain.user.UserType;
+import com.banksimplifiedbackend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void  validationTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT) {
+            throw new Exception("Merchant type user is not authorized to carry out transactions");
+        }
+
+        if (sender.getBalance().compareTo(amount) <= 0) {
+            throw new Exception("Insufficient funds");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("User not found"));
+    }
+
+    public void saveUser(User user) {
+        this.repository.save(user);
+    }
 }
