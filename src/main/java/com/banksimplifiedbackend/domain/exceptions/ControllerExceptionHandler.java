af8445baee9c9f30631675a58f0fdc1cc4a368db
Luan Verdelho <luanverdelho642@gmail.com>
2024-02-27 18:08:13 -0300

    feat: create exception rules

---
+++ src/main/java/com/banksimplifiedbackend/domain/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.banksimplifiedbackend.domain.exceptions;
+
+import com.banksimplifiedbackend.dtos.ExceptionDTO;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity threatDuplicateEntry(DataIntegrityViolationException exception) {
+        ExceptionDTO body = new ExceptionDTO("Already registered user", "400");
+        return ResponseEntity.badRequest().body(body);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity threat404(EntityNotFoundException exception) {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity threatGeneralException(Exception exception) {
+        ExceptionDTO body = new ExceptionDTO(exception.getMessage(), "500");
+        return ResponseEntity.internalServerError().body(body);
+    }
+}
---
+++ src/main/java/com/banksimplifiedbackend/dtos/ExceptionDTO.java
@@ -0,0 +1,4 @@
+package com.banksimplifiedbackend.dtos;
+
+public record ExceptionDTO(String message, String statusCode) {
+}
