e18d1bec217755b7485c55a31625111f4c0f20b1
Luan Verdelho <luanverdelho642@gmail.com>
2024-02-27 17:53:46 -0300

    fix: adjust notification service for request get method

--- src/main/java/com/banksimplifiedbackend/services/NotificationService.java
+++ src/main/java/com/banksimplifiedbackend/services/NotificationService.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+
 @Service
 public class NotificationService {
     @Autowired
@@ -17,11 +19,18 @@ public class NotificationService {
         String email = user.getEmail();
         NotificationDTO notificationRequest = new NotificationDTO(email, message);
 
-       ResponseEntity<String> notificationResponse = restTemplate.postForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6)", notificationRequest, String.class);
+        System.out.println("body request: "+ notificationRequest);
+        ResponseEntity<Map> notificationResponse = restTemplate.getForEntity("https://run.mocky.io/v3/54dc2cf1-3add-45b5-b5a9-6bf7e7f1f4a6", Map.class);
 
-       if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
-           System.out.println("Error sending notification");
-           throw new Exception("Notification service is down");
-       }
+        if (!(notificationResponse.getStatusCode() == HttpStatus.OK)) {
+            System.out.println("Error sending notification");
+            throw new Exception("Notification service is down");
+        }
+
+        boolean isApproved = (boolean) notificationResponse.getBody().get("message");
+
+        if (!isApproved) throw new Exception("Notification service is down");
+
+        System.out.println("Notification sent to the user");
     }
 }
